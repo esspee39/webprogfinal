70a6e1f0755eac37a9c0710049edc315211e5420
esspee <shadowphoenix39@gmail.com>
2021-06-24 02:16:16 -0400

    add stupid pictures of my face to the banner

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,9 @@ import './Banner.css';
 function Banner(){
   return (
     <div className = "Banner">
+      <img src ={logo} height="60" width="60"/>
       Owen's Pokemon App
+      <img className="flipped" src ={logo} height="60" width="60" />
     </div>
   );
 };
