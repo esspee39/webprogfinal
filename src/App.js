9d765766857f1cf14031353ffe7179131a8d757c
esspee <shadowphoenix39@gmail.com>
2021-06-24 01:56:56 -0400

    grab the first move from a given pokemon and parse it's type

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import logo from './nuwo.png';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from "axios";
 
-const url = "https://pokeapi.co/api/v2/pokemon/";
+const monURL = "https://pokeapi.co/api/v2/pokemon/";
+
+var moveURL;
 
 function getRandomID() {
   return Math.floor(Math.random() * (898 - 1) + 1);
 }
 
-function App(){
-  const [pokemonA, setPokemonA] = useState("bulbasaur");
-  const [pokemonDataA, setPokemonDataA] = useState([]);
+function firstUpper(text){
+  if(text.length == 0){
+    return text;
+  }
+  var newText = text[0].toUpperCase() + text.slice(1).toLowerCase();
+  return newText;
+}
 
-  const [pokemonB, setPokemonB] = useState("bulbasaur");
+function App(){
+  const [pokemonA, setPokemonA] = useState("");
+  const [pokemonDataA, setPokemonDataA] = useState([]);
+  const [pokemonB, setPokemonB] = useState("");
   const [pokemonDataB, setPokemonDataB] = useState([]);
+  const [moveType, setMoveType] = useState("");
+
+  useEffect(() => {
+    getMoveType()
+  }, [moveURL])
+
+  const getMoveType = () => {
+    axios.get(moveURL)
+    .then(response => {
+      console.log(response);
+      setMoveType(response.data.type.name);
+    })
+  }
 
   const handleChangeA = (e) => {
     setPokemonA(e.target.value.toLowerCase());
@@ -23,6 +45,11 @@ function App(){
   const handleSubmitA = (e) => {
     e.preventDefault();
     getPokemonA();
+  };
+
+  const handleButton = (e) => {
+    e.preventDefault();
+    console.log("BUTTON PUSHED");
   };
 
   const handleChangeB = (e) => {
@@ -37,8 +64,9 @@ function App(){
   const getPokemonA = async () => {
     const toArray = [];
     try {
-      const res = await axios.get(url+pokemonA);
+      const res = await axios.get(monURL+pokemonA);
       toArray.push(res.data);
+      console.log(toArray);
       setPokemonDataA(toArray);
     } catch (e) {
       console.log(e);
@@ -48,7 +76,7 @@ function App(){
   const getPokemonB = async () => {
     const toArray = [];
     try {
-      const res = await axios.get(url+pokemonB);
+      const res = await axios.get(monURL+pokemonB);
       toArray.push(res.data);
       setPokemonDataB(toArray);
     } catch (e) {
@@ -56,54 +84,70 @@ function App(){
     }
   };
 
-  console.log(pokemonDataA);
-  console.log(pokemonDataB);
+  //console.log(pokemonDataA);
+  //console.log(pokemonDataB);
 
   return (
     <div className = "App">
 
-      <div className="MonA">
-        <form onSubmit={handleSubmitA}>
-          <label>
-            <input
-              type="text"
-              onChange={handleChangeA}
-              placeholder="Enter Pokemon Name"
-            />
-          </label>
-        </form>
-        {pokemonDataA.map((data) => {
-          return (
-            <div className="container">
-             <img src={data.sprites.other["official-artwork"]["front_default"]} height="120" width="120"/>
-            </div>
-          );
-        })}
+      <div className="Mons">
+        <div className="MonA">
+          <form onSubmit={handleSubmitA}>
+            <label>
+              <input
+                type="text"
+                onChange={handleChangeA}
+                placeholder="Enter Pokemon Name"
+              />
+            </label>
+          </form>
+          {pokemonDataA.map((data) => {
+            return (
+              <div className="container">
+              <img src={data.sprites.other["official-artwork"]["front_default"]} height="120" width="120"/>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="MonB">
+          <form onSubmit={handleSubmitB}>
+            <label>
+              <input
+                type="text"
+                onChange={handleChangeB}
+                placeholder="Enter Pokemon Name"
+              />
+            </label>
+          </form>
+          {pokemonDataB.map((data) => {
+            console.log(moveURL);
+            return (
+              <div className="container">
+              <img src={data.sprites.other["official-artwork"]["front_default"]} height="120" width="120"/>
+              </div>
+            );
+          })}
+        </div>
       </div>
 
-      <div className="MonB">
-        <form onSubmit={handleSubmitB}>
-          <label>
-            <input
-              type="text"
-              onChange={handleChangeB}
-              placeholder="Enter Pokemon Name"
-            />
-          </label>
-        </form>
-        {pokemonDataB.map((data) => {
-          return (
-            <div className="container">
-             <img src={data.sprites.other["official-artwork"]["front_default"]} height="120" width="120"/>
-            </div>
-          );
-        })}
+      <div> 
+        <button onClick={handleButton}>
+          Generate Data
+       </button>
+        {pokemonDataA.map((data) => {
+            moveURL = data.moves[0].move.url;
+            return (
+              <div className="learns">
+              {firstUpper(data.name)} learns {firstUpper(data.moves[0].move.name)} which is a {firstUpper(moveType)}-type move.
+              </div>
+            );
+          })}
       </div>
 
     </div>
-    
+    //
   );
 };
-
 
 export default App;
